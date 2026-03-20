a8f0689af44bbaddbea523b2b7b46a5adb1384e8
Stuti Jagtap <stuti.jagtap@avantika.edu.in>
2026-03-20 17:10:59 +0530

    hehehe

---
+++ app/src/main/java/com/example/coffee5/splashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.coffee5
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffee5.databinding.ActivitySplashBinding
+
+class splashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startbutton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
